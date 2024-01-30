1ce88f9ae2329b3c139a6b2a9afe158a24efaaad
saidtalfana <saidtalfana1@gmail.com>
2024-01-30 15:27:15 +0100

    version01

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+typedef struct {
+    char description[100];
+    char priorite;
+    int jour;
+    int mois;
+    int annee;
+} Tache;
+
+
+void ajouter_tache() {};
+void afficher_tache() {};
+void modifier_tache() {};
+void supprimer_tache() {};
+
+int main() {
+    int option;
+    
+    do {
+        printf("***Choisir une operation***\n");
+        printf("1: Ajouter une tache\n");
+        printf("2: Afficher les taches\n");
+        printf("3: Modifier une tache\n");
+        printf("4: Supprimer une tache\n");
+        printf("5: Ordonner les taches\n");
+        printf("6: Filtrer les taches\n");
+        printf("7: Quitter\n");
+
+        do {
+            printf("Donnez votre choix: ");
+            scanf("%d", &option);
+            if (option > 7 || option < 1)
+                printf("Choix invalide\n");
+        } while (option > 7 || option < 1);
+
+        switch (option) {
+            case 1:
+                ajouter_tache();
+                break;
+            case 2:
+                afficher_tache();
+                break;
+            case 3:
+                modifier_tache();
+                break;
+            case 4:
+                supprimer_tache();
+                break;
+            case 5:
+              
+                break;
+            case 6:
+              
+                break;
+        
+        }
+    } while (option != 7);
+
+    return 0;
+}
